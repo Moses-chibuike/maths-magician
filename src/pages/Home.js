2b3ea81aa462d7e2a9ad749c829953a6a9ff62d4
zeusmoney <ezechukwuchibuikemoses@gmail.com>
2023-03-29 14:21:38 +0100

    fix linters

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,10 +4,9 @@ import React from 'react';
 const Home = () => (
   <div className="home">
     <h3 className="home-intro">Welcome to our page!</h3>
-    <p className="home-text">Math magicians" is a website for all students and fans of mathematics. It is a Single Page App (SPA) that allows users to:</p>
+    <p className="home-text">Math magicians is a website for all students and fans of mathematics. It is a Single Page App (SPA) that allows users to:</p>
     <p className="home-text">Make simple calculations.</p>
     <p className="home-text">Read and sahre a random math-related quote.</p>
-    <p className="home-text">Create to-do list for the day</p>
   </div>
 );
 export default Home;
